5ab6c93b7830b7138139628bbd6fb2e08502cf5d
Adowrath <cyrill.brunner@hispeed.ch>
2017-01-13 23:58:04 +0100

    Extracted menu functionality into a common superclass

---
+++ src/main/java/ch/bbbaden/idpa/bru_eap_mey/quiz/controllers/MainMenuController.java
@@ -0,0 +1,76 @@
+package ch.bbbaden.idpa.bru_eap_mey.quiz.controllers;
+
+import org.eclipse.jdt.annotation.Nullable;
+
+
+import ch.bbbaden.idpa.bru_eap_mey.quiz.model.QuizModel;
+
+/**
+ * Der Hauptcontroller, der allen Controllern, die das Menü
+ * darstellen, unterliegt.
+ */
+public abstract class MainMenuController {
+	
+	/**
+	 * Das Quizmodel
+	 */
+	private @Nullable QuizModel quizModel;
+	
+	/**
+	 * Initialisiert das Quizmodel.
+	 * 
+	 * @param model
+	 *        das Model
+	 */
+	public final void setModel(QuizModel model) {
+		this.quizModel = model;
+		this.signalDataLoaded();
+	}
+	
+	/**
+	 * @return
+	 * 		das Quizmodel dieses Controllers
+	 */
+	public final QuizModel getModel() {
+		assert this.quizModel != null;
+		return this.quizModel;
+	}
+	
+	/**
+	 * Diese Methode wird aufgerufen, wenn Daten geladen wurden.
+	 */
+	public void signalDataLoaded() { /* */ }
+	
+	/**
+	 * Öffnet einen Dialog zur Auswahl einer Datei, in welche
+	 * gespeichert werden soll.
+	 */
+	public void saveData() {
+		this.getModel().saveDataDialog();
+	}
+	
+	/**
+	 * Öffnet einen Dialog zur Auswahl einer Datei, aus welcher
+	 * geladen werden soll.
+	 */
+	public void loadData() {
+		if(this.getModel().loadDataDialog()) {
+			this.signalDataLoaded();
+		}
+	}
+	
+	/**
+	 * Ruft den Dialog zum Bearbeiten der Kategorien auf.
+	 */
+	public void editCategories() {
+		this.getModel().openCategoryEditor();
+	}
+	
+	/**
+	 * Ruft den Dialog zum Bearbieten der Fragen auf.
+	 */
+	public void editQuestions() {
+		this.getModel().openQuestionEditor();
+	}
+	
+}
--- src/main/java/ch/bbbaden/idpa/bru_eap_mey/quiz/controllers/QuestionEditController.java
+++ src/main/java/ch/bbbaden/idpa/bru_eap_mey/quiz/controllers/QuestionEditController.java
@@ -1,36 +1,8 @@
 package ch.bbbaden.idpa.bru_eap_mey.quiz.controllers;
 
-import org.eclipse.jdt.annotation.Nullable;
-
-
-import ch.bbbaden.idpa.bru_eap_mey.quiz.model.QuizModel;
-
 /**
  * Der Controller für die Übersicht der Fragen.
  */
-public class QuestionEditController {
-	
-	/**
-	 * Das Quizmodel
-	 */
-	private @Nullable QuizModel quizModel;
-	
-	/**
-	 * Initialisiert das Quizmodel.
-	 * 
-	 * @param model
-	 *        das Model
-	 */
-	public final void setModel(QuizModel model) {
-		this.quizModel = model;
-	}
-	
-	/**
-	 * @return
-	 * 		das Quizmodel dieses Controllers
-	 */
-	public final QuizModel getModel() {
-		assert this.quizModel != null;
-		return this.quizModel;
-	}
+public class QuestionEditController extends MainMenuController {
+	//
 }
